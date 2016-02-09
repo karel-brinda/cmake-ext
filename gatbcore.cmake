16f99da2881caf92d6bf1f82c706a5d6e6f92be0
Karel Břinda <karel.brinda@gmail.com>
2016-02-09 16:30:07 +0100

    Minifix.

--- gatbcore.cmake
+++ gatbcore.cmake
@@ -6,7 +6,7 @@ ExternalProject_Add(gatbcore
     URL "http://gatb-core.gforge.inria.fr/versions/src/gatb-core-1.1.0-Source.tar.gz"
     #GIT_TAG "tag"
     INSTALL_DIR ${gatbcore_INSTALL}
-    CMAKE_ARGS ${gatbcore_PREFIX}/src/gatbcore/gatb-core
+    CMAKE_ARGS
         -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
         -DCMAKE_INSTALL_PREFIX=${gatbcore_INSTALL}
 )
